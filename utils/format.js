ea856c0ef855568a15c913c7cf3905cdb3696a26
Mikephie <131840160+Mikephie@users.noreply.github.com>
2025-09-06 23:43:44 +0800

    Update format.js

--- utils/format.js
+++ utils/format.js
@@ -4,10 +4,12 @@ import { parse } from "@babel/parser";
 import generate from "@babel/generator";
 
 export function prettyFormat(src) {
+  // 先试 Prettier
   try {
     return prettier.format(src, { parser: "babel" });
   } catch {}
 
+  // 再用 Babel AST 重排
   try {
     const ast = parse(src, {
       sourceType: "unambiguous",
@@ -15,8 +17,9 @@ export function prettyFormat(src) {
       allowAwaitOutsideFunction: true,
       plugins: ["jsx", "classProperties", "optionalChaining", "dynamicImport"]
     });
-    return generate.default(ast, { retainLines: false, compact: false }).code;
+    return generate(ast, { retainLines: false, compact: false }).code;
   } catch {}
 
+  // 全部失败就原样返回
   return src;
 }
